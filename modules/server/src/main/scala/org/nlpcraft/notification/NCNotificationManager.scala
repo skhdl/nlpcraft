02f403c2bf0abd8e4df8b10b3936cd2534b97ce6
Aaron Radzinzski <aradzinski@datalingvo.com>
2018-12-12 19:38:19 -0800

    WIP on notification manager.

--- modules/server/src/main/scala/org/nlpcraft/notification/NCNotificationManager.scala
+++ modules/server/src/main/scala/org/nlpcraft/notification/NCNotificationManager.scala
@@ -27,21 +27,37 @@
 package org.nlpcraft.notification
 
 import org.nlpcraft.{NCConfigurable, NCLifecycle}
+
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Push-based notification manager.
   */
 object NCNotificationManager extends NCLifecycle("Notification manager") {
+    case class Event(
+        name: String,
+        params: Seq[(String, Any)]
+    )
+    
     private object Config extends NCConfigurable {
         private val CFG_PATH = "notification.endpoints"
     
         private val epList: List[String] = if (hocon.hasPath(CFG_PATH)) hocon.getStringList(CFG_PATH).asScala.toList else Nil
         
         val flushMsec = hocon.getLong("notification.flushSecs") * 1000
+        val maxBufferSize = hocon.getInt("notification.maxBufferSize")
+    
+        override def check(): Unit = {
+            require(flushMsec > 0 , s"flush interval ($flushMsec) must be > 0")
+            require(maxBufferSize > 0 , s"maximum buffer size ($maxBufferSize) must be > 0")
+        }
     }
     
     Config.check()
+    
+    // Bounded buffer of events to be flushed.
+    private val evts: ArrayBuffer[Event] = new ArrayBuffer[Event](Config.maxBufferSize)
     
     /**
       * Adds event with given name and optional parameters to the buffer. Buffer will be pushed to configured
@@ -52,8 +68,22 @@ object NCNotificationManager extends NCLifecycle("Notification manager") {
       */
     def addEvent(evtName: String, params: (String, Any)*): Unit = {
         ensureStarted()
+    
+        evts.synchronized {
+            evts += Event(evtName, params)
+            
+            if (evts.size > Config.maxBufferSize)
+                flush()
+        }
+    }
+    
+    /**
+      * Flushes accumulated events, if any, to the registered URL endpoints.
+      */
+    private def flush(): Unit = {
+        evts.synchronized {
         
-        
+        }
     }
     
     override def start(): NCLifecycle = {
