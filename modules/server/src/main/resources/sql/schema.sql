db4cdcc2795c5f1fdbaed8e0063c2f4692789322
Aaron Radzinzski <aradzinski@datalingvo.com>
2018-12-16 16:05:05 -0800

    Added clean up to schema script.

--- modules/server/src/main/resources/sql/schema.sql
+++ modules/server/src/main/resources/sql/schema.sql
@@ -24,6 +24,17 @@
 --         /_/
 --
 
+--
+-- Drop all existing user tables.
+--
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
+
 SET client_min_messages TO WARNING;
 
 --
