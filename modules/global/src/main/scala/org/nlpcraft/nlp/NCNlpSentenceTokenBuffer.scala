ed51a9b8d972fc28d27ca59c9a434105074d895f
Aaron Radzinzski <aradzinski@datalingvo.com>
2019-01-08 14:17:29 -0800

    Few fixes.

--- modules/global/src/main/scala/org/nlpcraft/nlp/NCNlpSentenceTokenBuffer.scala
+++ modules/global/src/main/scala/org/nlpcraft/nlp/NCNlpSentenceTokenBuffer.scala
@@ -31,6 +31,7 @@
 
 package org.nlpcraft.nlp
 
+import scala.collection.{IndexedSeq ⇒ IdxSeq, Seq}
 import scala.collection.mutable.ArrayBuffer
 
 /**
@@ -40,8 +41,89 @@ import scala.collection.mutable.ArrayBuffer
 class NCNlpSentenceTokenBuffer(sz: Int) extends ArrayBuffer[NCNlpSentenceToken](sz) {
     def this() = this(16)
     
+    /** Stringified stems. */
     lazy val stems = map(_.stem).mkString(" ")
+    
+    /** Stem-based hashcode. */
     lazy val stemsHash = stems.hashCode()
+    
+    type SSOT = IdxSeq[IdxSeq[Option[NCNlpSentenceToken]]]
+    type SST = IdxSeq[IdxSeq[NCNlpSentenceToken]]
+    
+    /**
+      * Gets all sequential permutations of tokens in this NLP sentence.
+      *
+      * For example, if NLP sentence contains "a, b, c, d" tokens, then
+      * this function will return the sequence of following token sequences in this order:
+      * "a b c d"
+      * "a b c"
+      * "b c d"
+      * "a b"
+      * "b c"
+      * "c d"
+      * "a"
+      * "b"
+      * "c"
+      * "d"
+      *
+      * NOTE: this method will not return any permutations with a quoted token.
+      *
+      * @param stopWords Whether or not include tokens marked as stop words.
+      * @param maxLen Maximum number of tokens in the sequence.
+      * @param withQuoted Whether or not to include quoted tokens.
+      */
+    def tokenMix(
+        stopWords: Boolean = false,
+        maxLen: Int = Integer.MAX_VALUE,
+        withQuoted: Boolean = false
+    ): SST = {
+        val toks = filter(t ⇒ stopWords || (!stopWords && !t.isStopword))
+        
+        val res = (for (n ← toks.length until 0 by -1 if n <= maxLen) yield toks.sliding(n)).flatten
+        
+        if (withQuoted) res else res.filter(!_.exists(_.isQuoted))
+    }
+    
+    /**
+      * Gets all sequential permutations of tokens in this NLP sentence.
+      * This method is like a 'tokenMix', but with all combinations of stop-words (with and without)
+      *
+      * @param maxLen Maximum number of tokens in the sequence.
+      * @param withQuoted Whether or not to include quoted tokens.
+      */
+    def tokenMixWithStopWords(maxLen: Int = Integer.MAX_VALUE, withQuoted: Boolean = false): SST = {
+        /**
+          * Gets all combinations for sequence of mandatory tokens with stop-words and without.
+          *
+          * Example:
+          * 'A (stop), B, C(stop) → [A, B, C]; [A, B]; [B, C], [B]
+          * 'A, B(stop), C(stop) → [A, B, C]; [A, B]; [A, C], [A].
+          *
+          * @param toks Tokens.
+          */
+        def permutations(toks: Seq[NCNlpSentenceToken]): SST = {
+            def multiple(seq: SSOT, t: NCNlpSentenceToken): SSOT =
+                if (seq.isEmpty)
+                    if (t.isStopword) IdxSeq(IdxSeq(Some(t)), IdxSeq(None)) else IdxSeq(IdxSeq(Some(t)))
+                else {
+                    (for (subSeq ← seq) yield subSeq :+ Some(t)) ++
+                        (if (t.isStopword) for (subSeq ← seq) yield subSeq :+ None else Seq.empty)
+                }
+            
+            var res: SSOT = IdxSeq.empty
+            
+            for (t ← toks)
+                res = multiple(res, t)
+            
+            res.map(_.flatten).filter(_.nonEmpty)
+        }
+        
+        tokenMix(stopWords = true, maxLen, withQuoted).
+            flatMap(permutations).
+            filter(_.nonEmpty).
+            distinct.
+            sortBy(seq ⇒ (-seq.length, seq.head.index))
+    }
 }
 
 object NCNlpSentenceTokenBuffer {
