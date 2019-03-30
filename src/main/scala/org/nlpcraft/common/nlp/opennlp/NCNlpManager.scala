f249ba8cc7374d30a1202ce789790fca39eb0558
Aaron Radzinzski <aradzinski@datalingvo.com>
2019-03-30 16:36:21 +0300

    Quick fix.

--- src/main/scala/org/nlpcraft/common/nlp/opennlp/NCNlpManager.scala
+++ src/main/scala/org/nlpcraft/common/nlp/opennlp/NCNlpManager.scala
@@ -149,8 +149,9 @@ object NCNlpManager extends NCLifecycle("OpenNLP manager") {
         val ners: Map[Array[Int], String] =
             this.
                 synchronized {
-                    val res = nerFinders.map { case (finder, name) ⇒
-                        finder.find(words).flatMap(p ⇒ Range.inclusive(p.getStart, p.getEnd - 1)) → name
+                    val res = nerFinders.map {
+                        case (finder, name) ⇒
+                            finder.find(words).flatMap(p ⇒ Range.inclusive(p.getStart, p.getEnd - 1)) → name
                     }
 
                     nerFinders.keySet.foreach(_.clearAdaptiveData())
